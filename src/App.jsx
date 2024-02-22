9da16a37dea71a33994d40e42ce0f32d5ba1ba46
Benedict Chacko Mathew <90237831+The-Peacemaker@users.noreply.github.com>
2024-02-22 12:03:09 +0000

    Update personal information and images

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Benedict Chacko Mathew",
+  title: "PROGRAMMER",
+  email: "benedictcm1@gmail.com",
+  gitHub: "https://github.com/The-Peacemaker",
+  instagram: "https://www.instagram.com/benedict.chacko/",
+  linkedIn: "benedict chacko mathew",
 };
 
 const primaryColor = "#4E567E";
